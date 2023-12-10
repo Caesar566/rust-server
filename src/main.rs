2e6a19225853374cf0a6e3a232c4bbe058c97e69
Caesar566 <3496776235@qq.com>
2023-12-10 23:03:32 +0800

    modified:   Cargo.toml 	new file:   src/index.html 	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -34,20 +34,25 @@ first example
 //    .await
 // }
 
-async fn index() -> impl Responder {
-    "Hello world"
-}
 
-#[actix_web::main]
-async fn main() -> std::io::Result<{
-    HttpServer::new(|| {
-        App::new()
-          .service(
-            web::scope("/app")
-                .route("/index.html", web::get().to(index)),
-          )
-    })
-    .bind(("127.0.0.1",8080))?
-    .run()
-    .await
-}
+/*
+By app example
+*/
+
+// async fn index() -> impl Responder {
+//     "Hello  world!"
+// }
+
+// #[actix_web::main]
+// async fn main() -> std::io::Result<()>{
+//     HttpServer::new(|| {
+//         App::new()
+//           .service(
+//             web::scope("/app")
+//                 .route("/index.html", web::get().to(index)),
+//           )
+//     })
+//     .bind(("127.0.0.1",8080))?
+//     .run()
+//     .await
+// }
