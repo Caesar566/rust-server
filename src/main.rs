ac9ed8e165707866c82be51dbd5067db5c1531b6
Caesar566 <3496776235@qq.com>
2023-12-10 20:22:53 +0800

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,53 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
+/*
+first example
+*/
+// #[get("/")]
+// async fn hello() -> impl Responder {
+//     HttpResponse::Ok()
+//        .body("Hello, world!")
+// }
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok()
-       .body("Hello, world!")
-}
+// #[post("/echo")]
+// async fn echo() -> impl Responder {
+//     HttpResponse::Ok()
+//       .body("123")
+// }
 
-#[post("/echo")]
-async fn echo() -> impl Responder {
-    HttpResponse::Ok()
-      .body("123")
-}
+// async fn manual_hello() -> impl Responder {
+//     HttpResponse::Ok()
+//      .body("Hey there")
+// }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok()
-     .body("Hey there")
+// #[actix_web::main]
+// async fn main() -> std::io::Result<()> {
+//     HttpServer::new(|| {
+//         App::new()
+//            .service(hello)
+//            .service(echo)
+//            //.service(manual_hello)
+//            .route("/hey", web::get().to(manual_hello))
+//     })
+//    .bind("127.0.0.1:8080")?
+//    .run()
+//    .await
+// }
+
+async fn index() -> impl Responder {
+    "Hello world"
 }
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> std::io::Result<{
     HttpServer::new(|| {
         App::new()
-           .service(hello)
-           .service(echo)
-           //.service(manual_hello)
-           .route("/hey", web::get().to(manual_hello))
+          .service(
+            web::scope("/app")
+                .route("/index.html", web::get().to(index)),
+          )
     })
-   .bind("127.0.0.1:8080")?
-   .run()
-   .await
+    .bind(("127.0.0.1",8080))?
+    .run()
+    .await
 }
